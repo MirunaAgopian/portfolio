95c0fb434560ca14ea4a8570a33e667f84da1028
Miruna Agopian <agopian.miruna@yahoo.com>
2026-03-03 19:57:50 +0100

    created structure of the feedback section

--- scripts/db.js
+++ scripts/db.js
@@ -5,8 +5,8 @@ let projectsInfo = [
     projectTitle: "Join",
     projectIcon: "../assets/img/join_project_icon.png",
     projectOverlayDescription:
-      "JOIN gave me practical experience with advanced CS DOM manipulation, and interactive UI behavior. I implemented data handling with Firebase, including dynamic rendering, structured storage, real‑time updates, and form validation. The project pushed my skills and showed me how to build solid, data‑driven frontend features with confidence.",
-    projectTechnologies: ["HTML", "CSS", "JS", "Firebase"],
+      "JOIN gave me practical experience with advanced CS, DOM manipulation, and interactive UI behavior. I implemented data handling with Firebase, including dynamic rendering, structured storage, real‑time updates, and form validation. The project pushed my skills and showed me how to build solid, data‑driven frontend features with confidence.",
+    projectTechnologies: ["HTML |", "CSS |", "JS |", "Firebase"],
     projectBlackBoxDescription:
       "A collaborative Kanban board application built as a team project. It allows users to create tasks, assign them to contacts, prioritize them, and move them across categories using drag‑and‑drop.",
     projectGitHubLink: "https://github.com/MirunaAgopian/JOIN-2",
@@ -18,8 +18,8 @@ let projectsInfo = [
     projectTitle: "El pollo loco",
     projectIcon: "../assets/img/epl_project_icon.png",
     projectOverlayDescription:
-      "El Pollo Loco was my first real dive into 2D game development. I learned how to detect collisions horizontally and vertically, control animation timing with well‑structured intervals, clear them correctly, and manage sound effects without performance issues. It challenged me, but it also showed me how powerful OOP can be when building interactive experiences.",
-    projectTechnologies: ["HTML", "CSS", "JS - OOP"],
+      "El Pollo Loco was my first real dive into 2D game development. I learned how to detect collisions horizontally and vertically, control animation timing with well‑structured intervals, clear them correctly, and manage sound effects without performance issues. It challenged me, but it also showed me how powerful OOP can be when building interactive applications.",
+    projectTechnologies: ["HTML |", "CSS |", "JS - OOP"],
     projectBlackBoxDescription:
       "A 2D jump‑and‑run game built with JavaScript and object‑oriented programming, inspired by classic platformers. Guide the character through the world, collect coins and bottles, and face the final enemy.",
     projectGitHubLink: "https://github.com/MirunaAgopian/El-pollo-locco",

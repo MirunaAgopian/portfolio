76c4da2013fbdd869896def1ae957241ae4ce2b9
Miruna Agopian <agopian.miruna@yahoo.com>
2026-03-04 20:32:07 +0100

    fixed image paths for project deployment

--- scripts/db.js
+++ scripts/db.js
@@ -1,9 +1,9 @@
 let projectsInfo = [
   {
-    projectImage: "../assets/img/join.png",
+    projectImage: "assets/img/join.png",
     projectAlt: "Image of the kanbanboard project, JOIN",
     projectTitle: "Join",
-    projectIcon: "../assets/img/join_project_icon.png",
+    projectIcon: "assets/img/join_project_icon.png",
     projectOverlayDescription:
       "JOIN gave me practical experience with advanced CS, DOM manipulation, and interactive UI behavior. I implemented data handling with Firebase, including dynamic rendering, structured storage, real‑time updates, and form validation. The project pushed my skills and showed me how to build solid, data‑driven frontend features with confidence.",
     projectTechnologies: ["HTML |", "CSS |", "JS |", "Firebase"],
@@ -13,10 +13,10 @@ let projectsInfo = [
     projectLink: "",
   },
   {
-    projectImage: "../assets/img/epl2.png",
+    projectImage: "assets/img/epl2.png",
     projectAlt: "Image of the 2d jump and run game, El pollo loco",
     projectTitle: "El pollo loco",
-    projectIcon: "../assets/img/epl_project_icon.png",
+    projectIcon: "assets/img/epl_project_icon.png",
     projectOverlayDescription:
       "El Pollo Loco was my first real dive into 2D game development. I learned how to detect collisions horizontally and vertically, control animation timing with well‑structured intervals, clear them correctly, and manage sound effects without performance issues. It challenged me, but it also showed me how powerful OOP can be when building interactive applications.",
     projectTechnologies: ["HTML |", "CSS |", "JS - OOP"],

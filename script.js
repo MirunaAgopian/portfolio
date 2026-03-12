d45cbf1d7807baaad23aa303434623f62f4f0f98
Miruna Agopian <agopian.miruna@yahoo.com>
2026-03-12 18:59:56 +0100

    finished styling and funcitonalities on mobile navbar

--- script.js
+++ script.js
@@ -331,3 +331,37 @@ function initHeaderObserver() {
   );
   sections.forEach((section) => observer.observe(section));
 }
+
+function toggleMobileNav(isOpen) {
+  const nav = document.getElementById("mobie_nav");
+  if (isOpen) {
+    nav.classList.add("visible");
+  } else {
+    nav.classList.remove("visible");
+  }
+}
+
+document
+  .getElementById("burger_menu")
+  .addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleMobileNav(true)
+  });
+
+document
+  .getElementById("close_mobile_menu_btn")
+  .addEventListener("click", () => toggleMobileNav(false));
+
+document.addEventListener("click", (event) => {
+  const nav = document.getElementById("mobie_nav");
+  if(!nav.classList.contains("visible")) return;
+  if(nav.contains(event.target)) return;
+  toggleMobileNav(false);
+});
+
+const navLinks = document.querySelectorAll(".menu-options a");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    toggleMobileNav(false);
+  });
+})

7f1a085dc3f7949508f9e59008ad9b2a1a20e337
Miruna Agopian <agopian.miruna@yahoo.com>
2026-07-28 18:55:29 +0200

    shortened description for PollApp

--- scripts/db.js
+++ scripts/db.js
@@ -22,7 +22,7 @@ let projectsInfo = {
         "PollApp strengthened my Angular fundamentals through building a modular survey system with real‑time voting. I worked with Supabase for structured storage, CRUD operations, and live updates, while creating reusable components and clean UI utilities. The project was fun and deepened my understanding of scalable architecture and gave me confidence in building interactive, data‑driven frontend features.",
       projectTechnologies: ["Angular, TS |", "SCSS |", "Supabase"],
       projectBlackBoxDescription:
-        "A real‑time survey application built with Angular. It allows users to create surveys with multiple questions, vote instantly, and see live results powered by Supabase. The modular architecture includes reusable components, dynamic rendering, and structured data handling for fast, interactive user experiences.",
+        "A real‑time survey application. It allows users to create surveys with multiple questions, vote instantly, and see live results. It includes reusable components, dynamic rendering, and structured data handling for fast, interactive user experiences.",
       projectGitHubLink: "https://github.com/MirunaAgopian/PollApp",
       projectLink: "https://miruna-agopian.de/poll_app/",
     },
@@ -64,7 +64,7 @@ let projectsInfo = {
         "PollApp stärkte meine Angular‑Grundlagen durch den Aufbau eines modularen Umfragesystems mit Echtzeit‑Voting. Ich arbeitete mit Supabase für strukturierte Speicherung, CRUD‑Operationen und Live‑Updates und entwickelte wiederverwendbare Komponenten sowie UI‑Utilities. Das Projekt vertiefte mein Verständnis für skalierbare Architektur und gab mir Sicherheit beim Entwickeln interaktiver, datengetriebener Frontend‑Features.",
       projectTechnologies: ["Angular, TS |", "SCSS |", "Supabase"],
       projectBlackBoxDescription:
-        "Eine Echtzeit‑Umfrageanwendung, entwickelt mit Angular. Nutzer können Umfragen mit mehreren Fragen erstellen, sofort abstimmen und dank Supabase Live‑Ergebnisse sehen. Die modulare Architektur umfasst wiederverwendbare Komponenten, dynamisches Rendering und strukturierte Datenverarbeitung für schnelle, interaktive Nutzererlebnisse.",
+        "Eine Echtzeit‑Umfrageapp in der Nutzer Umfragen erstellen, sofort abstimmen und Live‑Ergebnisse sehen. Sie nutzt wiederverwendbare Komponenten, dynamisches Rendering und strukturierte Datenverarbeitung für schnelle, interaktive Erlebnisse.",
       projectGitHubLink: "https://github.com/MirunaAgopian/PollApp",
       projectLink: "https://miruna-agopian.de/poll_app/",
     },
